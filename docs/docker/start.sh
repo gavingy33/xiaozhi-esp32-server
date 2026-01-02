668a799463a136baaf59d9777a660be67fce6b19
sdweizan <gaofeng@caihong.com>
2026-01-02 09:33:36 +0800

    修改日志文件目录

--- docs/docker/start.sh
+++ docs/docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 启动Java后端（docker内监听8003端口）
-java -Dspring.profiles.active=prd -jar /app/xiaozhi-esp32-api.jar --server.port=8003 &
+java -DLOG_HOME=/app/logs -Dspring.profiles.active=prd -jar /app/xiaozhi-esp32-api.jar --server.port=8003 &
 
 # 启动Nginx（前台运行保持容器存活）
 nginx -g 'daemon off;'
